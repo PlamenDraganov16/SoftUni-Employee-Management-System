3636aea4010f4d4dd6d88576d5bd9c6e3b37ca3b
Plamen Draganov <plamine95@gmail.com>
2025-11-12 17:50:01 +0200

    show dynamic users

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import UserItem from "./UserItem.jsx";
 
 export default function UserList() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3030/jsonstore/users')
+            .then(response => response.json())
+            .then(result => {
+                setUsers(Object.values(result));
+            })
+            .catch((err) => alert(err.message));  
+    }, [])
+
     return (
         <div className="table-wrapper">
             {/* <!-- Overlap components  --> */}
@@ -64,7 +76,7 @@ export default function UserList() {
                 </thead>
                 <tbody>
                     {/* <!-- Table row component --> */}
-                    <UserItem />
+                    {users.map(user => <UserItem key = {user._id} {...user}/>)}
                 </tbody>
             </table>
 
