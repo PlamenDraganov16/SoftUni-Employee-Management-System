8e7f5eb7ef1bb171219b5128d62ca0f02f006beb
Plamen Draganov <plamine95@gmail.com>
2025-11-12 18:34:35 +0200

    refresh userPage after createnewuser

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CreateUserModal from './components/CreateUserModal.jsx'
 function App() {
     const [showCreateUser, setShowCreateUser] = useState(false);
     const [users, setUsers] = useState([]);
+    const [forceRefresh, setForceRefresh] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:3030/jsonstore/users')
@@ -17,7 +18,7 @@ function App() {
                 setUsers(Object.values(result));
             })
             .catch((err) => alert(err.message));
-    }, []);
+    }, [forceRefresh]);
 
     const addUserClickHandler = () => {
         setShowCreateUser(true);
@@ -49,10 +50,8 @@ function App() {
             },
             body: JSON.stringify(userData)
         })
-            .then(response => response.json())
-            .then(result => {
-                console.log(result)
-            })
+            .then(() => setForceRefresh(state => !state))
+            .catch(err => alert(err.message));
     }
 
     return (
@@ -63,7 +62,7 @@ function App() {
                 <section className="card users-container">
                     <SearchForm />
 
-                    <UserList users = {users}/>
+                    <UserList users={users} />
 
                     <button className="btn-add btn" onClick={addUserClickHandler}>Add new user</button>
 
